44d6082bee79320b33d88d23d327ecb971ae5d6b
Marwil Campos <marwilcampos@gmail.com>
2021-02-28 20:22:21 -0400

    :sparkles: Add transparent background

--- 02-components/src/app/app.component.ts
+++ 02-components/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { IComponent } from './interfaces/component';
+import { DataService } from './services/data.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+    components: Observable<IComponent[]>;
+
+    constructor(
+        private _menu: MenuController,
+        private dataService: DataService
+    ) {}
+
+    ngOnInit() {
+        this.components = this.dataService.getMenuOpts();
+    }
 }
