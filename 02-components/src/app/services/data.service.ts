5648e4e6b17e2c2aa45d3a1d0a2f15354eda6ab2
Marwil Campos <marwilcampos@gmail.com>
2021-03-05 22:09:41 -0400

    :sparkles: Add skeleton text

--- 02-components/src/app/services/data.service.ts
+++ 02-components/src/app/services/data.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { IComponent } from '../interfaces/component';
-
 @Injectable({
     providedIn: 'root',
 })
@@ -28,8 +28,8 @@ export class DataService {
     }
 
     getHeros(): Observable<IComponent[]> {
-        return this._http.get<IComponent[]>(
-            '/assets/data/superheroes.json'
-        );
+        return this._http
+            .get<IComponent[]>('/assets/data/superheroes.json')
+            .pipe(delay(1500));
     }
 }
