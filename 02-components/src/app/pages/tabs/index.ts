50aaf5c57dc2191e9443d5ca1c4ab898b652aeaa
Marwil Campos <marwilcampos@gmail.com>
2021-03-05 23:27:36 -0400

    :sparkles: Add tabs

---
+++ 02-components/src/app/pages/tabs/index.ts
@@ -0,0 +1,3 @@
+export * from './tabs-routing.module';
+export * from './tabs.module';
+export * from './tabs.page';
---
+++ 02-components/src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/tabs/account',
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: TabsPage,
+        children: [
+            {
+                path: 'account',
+                loadChildren: () =>
+                    import('../avatar/avatar.module').then(
+                        (m) => m.AvatarPageModule
+                    ),
+            },
+            {
+                path: 'contact',
+                loadChildren: () =>
+                    import('../list/list.module').then(
+                        (m) => m.ListPageModule
+                    ),
+            },
+            {
+                path: 'settings',
+                loadChildren: () =>
+                    import(
+                        '../infinity-scroll/infinity-scroll.module'
+                    ).then((m) => m.InfinityScrollPageModule),
+            },
+        ],
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
