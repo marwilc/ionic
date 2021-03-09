b3b5548a6cc45c514a505087e71f7199f66da010
Marwil Campos <marwilcampos@gmail.com>
2021-03-08 22:23:29 -0400

    :sparkles: Add segment headers

--- 03-news/src/app/pages/tab2/tab2.page.ts
+++ 03-news/src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit, AfterViewInit {
+  @ViewChild(IonSegment) segment: IonSegment;
+  categories = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'sciences',
+    'ports',
+    'technology',
+  ];
 
   constructor() {}
+  ngAfterViewInit(): void {
+    console.log(this.segment);
+    this.segment.value = this.categories[0];
+  }
 
+  ngOnInit() {
+    console.log(this.segment);
+    //this.segment.value = this.categories[0];
+  }
 }
