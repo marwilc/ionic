fab7b3d33c3d050a3cf92542970264e4057e5683
Marwil Campos <marwilcampos@gmail.com>
2021-03-08 23:17:00 -0400

    :sparkles: Add load news

--- 03-news/src/app/pages/tab2/tab2.module.ts
+++ 03-news/src/app/pages/tab2/tab2.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ComponentsModule } from 'src/app/components/components.module';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 import { Tab2Page } from './tab2.page';
@@ -11,6 +12,7 @@ import { Tab2Page } from './tab2.page';
     IonicModule,
     CommonModule,
     FormsModule,
+    ComponentsModule,
     ExploreContainerComponentModule,
     Tab2PageRoutingModule,
   ],
--- 03-news/src/app/pages/tab2/tab2.page.ts
+++ 03-news/src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,13 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { Subscription } from 'rxjs';
+import { Article } from 'src/app/interfaces/interfaces';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
@@ -8,8 +15,9 @@ import { NewsService } from 'src/app/services/news.service';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page implements OnInit, AfterViewInit {
+export class Tab2Page implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(IonSegment) segment: IonSegment;
+
   categories = [
     'business',
     'entertainment',
@@ -20,25 +28,38 @@ export class Tab2Page implements OnInit, AfterViewInit {
     'technology',
   ];
 
+  news: Article[] = [];
+
   private _subscription = new Subscription();
 
   constructor(private _news: NewsService) {}
 
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+
   ngAfterViewInit(): void {
     console.log(this.segment);
     this.segment.value = this.categories[0];
-
-    this._subscription.add(
-      this._news
-        .getTopHeadlinesCategory(this.categories[0])
-        .subscribe((resp) => {
-          console.log(resp);
-        })
-    );
+    this.loadNews(this.categories[0]);
   }
 
   ngOnInit() {
     console.log(this.segment);
-    //this.segment.value = this.categories[0];
+  }
+
+  onChangeSegment(event) {
+    const { detail } = event;
+    this.news = [];
+    this.loadNews(detail.value);
+  }
+
+  loadNews(category: string) {
+    this._subscription.add(
+      this._news.getTopHeadlinesCategory(category).subscribe((resp) => {
+        console.log(resp);
+        this.news.push(...resp.articles);
+      })
+    );
   }
 }
