90d1ca898db7fb290de55f3d38fe7dfe4aa639bd
Marwil Campos <marwilcampos@gmail.com>
2021-03-10 23:23:36 -0400

    :sparkles: Add action sheet

--- 03-news/src/app/components/new/new.component.ts
+++ 03-news/src/app/components/new/new.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Plugins } from '@capacitor/core';
+import { ActionSheetController } from '@ionic/angular';
 import { Article } from 'src/app/interfaces/interfaces';
 
 const { Browser } = Plugins;
@@ -12,11 +13,42 @@ const { Browser } = Plugins;
 export class NewComponent implements OnInit {
   @Input() new: Article;
   @Input() index: number;
-  constructor() {}
+
+  constructor(private _actionSheet: ActionSheetController) {}
 
   ngOnInit() {}
 
   async showNew() {
     await Browser.open({ url: this.new.url });
+  }
+
+  async launchMenu() {
+    const actionSheet = await this._actionSheet.create({
+      buttons: [
+        {
+          text: 'Share',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+          },
+        },
+        {
+          text: 'Favorites',
+          icon: 'star',
+          handler: () => {
+            console.log('Favorites');
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
   }
 }
