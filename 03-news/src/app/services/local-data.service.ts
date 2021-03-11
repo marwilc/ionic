b37bb5596adb9db7537b0c521858c48ae4992461
Marwil Campos <marwilcampos@gmail.com>
2021-03-11 00:11:31 -0400

    :sparkles: Add storage

---
+++ 03-news/src/app/services/local-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { Article } from '../interfaces/interfaces';
+
+const { Storage } = Plugins;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalDataService {
+  news: Article[] = [];
+
+  constructor() {}
+
+  async saveNew(notice: Article) {
+    const exist = this.news.find((n) => n.title === notice.title);
+
+    if (!exist) {
+      this.news.unshift(notice);
+      await Storage.set({ key: 'favorites', value: JSON.stringify(this.news) });
+    }
+  }
+}
