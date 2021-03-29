aae9068184bd62cc2e7cbabb7dea58db1d9e2a4a
Marwil Campos <marwilcampos@gmail.com>
2021-03-28 20:00:20 -0400

    :sparkles: add get actors

--- 04-moviesApp/src/app/components/detail/detail.component.ts
+++ 04-moviesApp/src/app/components/detail/detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
-import { Movie } from 'src/app/interfaces/interfaces';
+import { Cast, Movie } from 'src/app/interfaces/interfaces';
 import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
@@ -11,10 +12,21 @@ import { MoviesService } from 'src/app/services/movies.service';
 export class DetailComponent implements OnInit, OnDestroy {
   @Input() id: string;
   movie: Movie;
+  actors: Cast[] = [];
+  hidden = 150;
+
+  slideOptions = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5,
+  };
 
   private _subscription = new Subscription();
 
-  constructor(private _movies: MoviesService) {}
+  constructor(
+    private _movies: MoviesService,
+    private _modal: ModalController
+  ) {}
 
   ngOnDestroy(): void {
     this._subscription.add(this._subscription.unsubscribe());
@@ -28,9 +40,15 @@ export class DetailComponent implements OnInit, OnDestroy {
     );
 
     this._subscription.add(
-      this._movies.getActors(this.id).subscribe((data) => {
-        console.log(data);
+      this._movies.getActors(this.id).subscribe((credits) => {
+        this.actors = credits.cast;
       })
     );
   }
+
+  back() {
+    this._modal.dismiss();
+  }
+
+  favorite() {}
 }
