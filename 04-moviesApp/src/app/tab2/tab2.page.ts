fd558262b70a62874cda89377dc59496dff85e1b
Marwil Campos <marwilcampos@gmail.com>
2021-03-28 21:10:20 -0400

    :sparkles: add searchbar

--- 04-moviesApp/src/app/tab2/tab2.page.ts
+++ 04-moviesApp/src/app/tab2/tab2.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
+  textSearch = '';
+  ideas: string[] = ['Spiderman', 'Avengers', 'El señor de los anillos'];
   constructor() {}
 
+  onSearchChange(event) {
+    console.log(event);
+  }
+
+  pasteToSearch(text: string) {
+    this.textSearch = text;
+  }
 }
