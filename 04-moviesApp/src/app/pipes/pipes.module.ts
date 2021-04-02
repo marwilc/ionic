ebb19256241ac61bc6d3433d9d4f13a25fb48a48
Marwil Campos <marwilcampos@gmail.com>
2021-04-02 16:42:31 -0400

    :sparkles: add component spinner

--- 04-moviesApp/src/app/pipes/pipes.module.ts
+++ 04-moviesApp/src/app/pipes/pipes.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FilterBackdropPipe } from './filter-backdrop.pipe';
 import { ImagePipe } from './image.pipe';
 import { ParPipe } from './par.pipe';
 
 @NgModule({
-  declarations: [ImagePipe, ParPipe],
+  declarations: [ImagePipe, ParPipe, FilterBackdropPipe],
   imports: [CommonModule],
-  exports: [ImagePipe, ParPipe],
+  exports: [ImagePipe, ParPipe, FilterBackdropPipe],
 })
 export class PipesModule {}
