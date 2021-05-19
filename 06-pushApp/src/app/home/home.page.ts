2a6e8474a8dc8e79202fb5c896e82174af4f1fdf
Marwil Campos <marwilcampos@gmail.com>
2021-05-19 07:30:11 -0400

    :sparkles: add delete messages

--- 06-pushApp/src/app/home/home.page.ts
+++ 06-pushApp/src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
+import { OSNotificationPayload } from '@ionic-native/onesignal/ngx';
+import { PushService } from '../services/push.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  messages: OSNotificationPayload[] = [];
 
-  constructor() {}
+  constructor(
+    public push: PushService,
+    private applicationRef: ApplicationRef
+  ) {}
 
+  ngOnInit(): void {
+    this.push.pushListener.subscribe((push) => {
+      this.messages.unshift(push);
+      this.applicationRef.tick();
+    });
+  }
+
+  async ionViewWillEnter() {
+    this.messages = await this.push.getMessages();
+  }
+
+  async deleteMessages() {
+    await this.push.deleteMessages();
+    this.messages = [];
+  }
 }
--- 06-pushApp/src/app/services/push.service.ts
+++ 06-pushApp/src/app/services/push.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+/* eslint-disable no-underscore-dangle */
+import { EventEmitter, Injectable } from '@angular/core';
+import {
+  OneSignal,
+  OSNotification,
+  OSNotificationPayload,
+} from '@ionic-native/onesignal/ngx';
 import { environment } from 'src/environments/environment';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PushService {
-  constructor(private oneSignal: OneSignal) {}
+  messages: OSNotificationPayload[] = [];
+  pushListener = new EventEmitter<OSNotificationPayload>();
+  userId: string;
+
+  constructor(private oneSignal: OneSignal, private _storage: StorageService) {
+    this.loadMessages();
+  }
+
+  async getMessages() {
+    await this.loadMessages();
+    return [...this.messages];
+  }
 
   initSetup() {
     this.oneSignal.startInit(
@@ -15,19 +32,69 @@ export class PushService {
     );
 
     this.oneSignal.inFocusDisplaying(
-      this.oneSignal.OSInFocusDisplayOption.InAppAlert
+      this.oneSignal.OSInFocusDisplayOption.Notification
     );
 
-    this.oneSignal.handleNotificationReceived().subscribe((push) => {
+    this.oneSignal.handleNotificationReceived().subscribe(async (push) => {
       // do something when notification is received
       console.log('Push received', push);
+      await this.pushReceived(push);
     });
 
-    this.oneSignal.handleNotificationOpened().subscribe((push) => {
+    this.oneSignal.handleNotificationOpened().subscribe(async (push) => {
       // do something when a notification is opened
       console.log('Tap push', push);
+      await this.pushReceived(push.notification);
+    });
+
+    //get id of subscriber
+    this.oneSignal.getIds().then((info) => {
+      this.userId = info.userId;
     });
 
     this.oneSignal.endInit();
+  }
+
+  async pushReceived(push: OSNotification) {
+    await this.loadMessages();
+    const payload = push.payload;
+
+    const existPush = this.messages.find(
+      (message) => message.notificationID === payload.notificationID
+    );
+
+    if (existPush) {
+      return;
+    }
+
+    this.messages.unshift(payload);
+    this.pushListener.emit(payload);
+    await this.saveMessages();
+  }
+
+  /**
+   * save messages
+   *
+   * @memberof PushService
+   */
+  async saveMessages() {
+    await this._storage.set('messages', this.messages);
+  }
+
+  /**
+   * load messages
+   *
+   * @memberof PushService
+   */
+  async loadMessages() {
+    this.messages = (await this._storage.get('messages')) || [];
+
+    return this.messages;
+  }
+
+  async deleteMessages() {
+    await this._storage.clear();
+    this.messages = [];
+    await this.saveMessages();
   }
 }
